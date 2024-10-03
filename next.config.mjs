fd52bbf8ef736f88110766c13c28dd07e3acbbba
Phuoc Nguyen <phuocnguyen@Phuocs-MacBook-Pro.local>
2024-10-03 17:42:24 +0700

    eslint: {
            ignoreDuringBuilds: true,
        },

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-        {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      }, {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            }, {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
