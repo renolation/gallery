6e64b86884dd45a5e4fa9a00bd8a789352472b0c
Phuoc Nguyen <phuocnguyen@Phuocs-MacBook-Pro.local>
2024-10-03 18:06:34 +0700

    aasdasdasdxcasdasd

--- app/posts/[postId]/edit/page.tsx
+++ app/posts/[postId]/edit/page.tsx
@@ -6,28 +6,10 @@ import CreatePostPanel from "@/components/post/create/create-post-panel";
 import {Status} from "@prisma/client";
 import {Image as ImageDB} from "@prisma/client";
 
-type Tag = {
-    id: string;
-    name: string;
-};
-
-type Post = {
-    id: string;
-    title: string | null;
-    description: string | null;
-    userId: string;
-    status: Status;
-    createdAt: Date;
-    updatedAt: Date;
-    images: ImageDB[],
-    tags: { tag: Tag }[];
-};
 
 export default async function EditPostPage({params}: { params: { postId: string } }) {
-        const post: Post | null = await getPostById(params.postId);
 
-
-
+    const post = await getPostById(params.postId);
 
 
     if (!post) {
